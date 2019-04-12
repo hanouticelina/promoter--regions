33751552427004e12caadaefb2d1b095d2822dd8
celina <hanouticelina@outlook.com>
2019-04-12 17:29:15 +0200

    edited plot_histograms but bugging

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 from functools import reduce
 from itertools import product
-
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -345,15 +345,16 @@ def plot_histogram(sequence,freqs, nb_simulation=1000):
     pik = stationary_distribution(freqs, transition_matrix(sequence), 0.00001)
     for word in words:
         p_emp[word] = p_empirique(len(sequence), word, freqs, nb_simulation)
-        p[word] = markov_proba(word,transition_matrix(sequence),pik)
+        p[word] = markov_proba(str_to_int(word),transition_matrix(sequence),pik)
     fig, axes = plt.subplots(2, 2, figsize=(15, 15))
     for pos, word in zip(positions, p_emp.keys()):
         max_ = max(p_emp[word].keys()) + 1
         k = np.arange(max_)
-        l = p * (len(sequence) - len(word) + 1)
+        l = p[word] * (len(sequence) - len(word) + 1)
         keys = np.array([x for x in p_emp[word].keys()])
         values = np.array([x for x in p_emp[word].values()])
-        axes[pos].scatter(k,((l**k)/factorial(k))*np.exp(-l), zorder=2)
+        poisson = [(l**i)/math.factorial(i)*np.exp(l) for i in k]
+        axes[pos].scatter(k,poisson, zorder=2)
         axes[pos].bar(keys, values)
         axes[pos].grid(True)
         axes[pos].set_title("Distribution des occurrences de " + word)
